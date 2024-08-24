b8adda8c7e3c1b20dfae263ffcf8ee1559d416c1
Sviatlana Viarbitskaya <sviatlana.viarbitskaya@gmail.com>
2024-08-24 12:54:35 +0200

    Add basic form types, CRUD controllers, and templates
    - Created basic `User` and `Group` form types.
    - Implemented basic CRUD controller actions for `User` and `Group`.
    - Added empty templates corresponding to new actions.
    - Renamed some templates for convenience.
    - Installed Symfony Form and Webpack Encore.
    - Upgraded dependencies to the latest versions.

--- src/Controller/Admin/GroupController.php
+++ src/Controller/Admin/GroupController.php
@@ -2,16 +2,84 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Group;
+use App\Form\GroupType;
+use App\Repository\GroupRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GroupController extends AbstractController
 {
-    #[Route('/admin/groups', name: 'admin_groups')]
-    public function groups(): Response
+    #[Route('/admin/groups', name: 'admin_groups', methods: ['GET'])]
+    public function groups(GroupRepository $groupRepository): Response
     {
-        // Render groups management page
-        return $this->render('admin/groups.html.twig');
+        // Fetch the list of groups from the database
+        $groups = $groupRepository->findAll();
+
+        // Render the template and pass the list of groups to it
+        return $this->render('admin/group/list.html.twig', [
+            'groups' => $groups,
+        ]);
+    }
+
+    #[Route('/admin/group/{id}', name: 'admin_group_show', methods: ['GET'])]
+    public function show(Group $group): Response
+    {
+        // Render the template and pass the group to it
+        return $this->render('admin/group/show.html.twig', [
+            'group' => $group,
+        ]);
+    }
+
+    #[Route('/admin/group/new', name: 'admin_group_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $group = new Group();
+        $form = $this->createForm(GroupType::class, $group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($group);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_groups');
+        }
+
+        return $this->render('admin/group/new.html.twig', [
+            'group' => $group,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/group/{id}/edit', name: 'admin_group_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Group $group, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(GroupType::class, $group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_groups');
+        }
+
+        return $this->render('admin/group/edit.html.twig', [
+            'group' => $group,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/group/{id}', name: 'admin_group_delete', methods: ['POST'])]
+    public function delete(Request $request, Group $group, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$group->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($group);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_groups');
     }
 }
--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,22 +2,68 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\UserRepository;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UserController extends AbstractController
 {
-    #[Route('/admin/users', name: 'admin_users')]
+    #[Route('/admin/users', name: 'admin_users', methods: ['GET'])]
     public function users(UserRepository $userRepository): Response
     {
         // Fetch the list of users from the database
         $users = $userRepository->findAll();
 
         // Render the template and pass the list of users to it
-        return $this->render('admin/users.html.twig', [
+        return $this->render('admin/user/list.html.twig', [
             'users' => $users,
         ]);
+    }
+
+    #[Route('/admin/user/{id}', name: 'admin_user_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        // Render the template and pass the user to it
+        return $this->render('admin/user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/admin/user/{id}/edit', name: 'admin_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, $user)
+            ->add('saveAndCreateNew', SubmitType::class)
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_users');
+        }
+
+        return $this->render('admin/user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/user/{id}', name: 'admin_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_users');
     }
 }
