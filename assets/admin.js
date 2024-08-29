a2b53cf15d30607f9115f439e7508c9d4b9f650b
Sviatlana Viarbitskaya <sviatlana.viarbitskaya@gmail.com>
2024-08-29 11:06:15 +0200

    Installed datatables.net-dt

--- assets/admin.js
+++ assets/admin.js
@@ -1,22 +1,32 @@
 import $ from 'jquery';
+import DataTable from 'datatables.net-dt';
 
-// Handling the modal confirmation message.
 
-$(document).on('submit', 'form[data-confirmation]', function (event) {
-    var $form = $(this),
-        $confirm = $('#confirmationModal');
+$(document).ready( function () {
 
-    if ($confirm.data('result') !== 'yes') {
-        //cancel submit event
-        event.preventDefault();
+    // Handling the modal confirmation message.
+    $(document).on('submit', 'form[data-confirmation]', function (event) {
+        var $form = $(this),
+            $confirm = $('#confirmationModal');
 
-        $confirm
-            .off('click', '#btnYes')
-            .on('click', '#btnYes', function () {
-                $confirm.data('result', 'yes');
-                $form.find('input[type="submit"]').attr('disabled', 'disabled');
-                $form.trigger('submit');
-            })
-            .modal('show');
-    }
-});
+        if ($confirm.data('result') !== 'yes') {
+            //cancel submit event
+            event.preventDefault();
+
+            $confirm
+                .off('click', '#btnYes')
+                .on('click', '#btnYes', function () {
+                    $confirm.data('result', 'yes');
+                    $form.find('input[type="submit"]').attr('disabled', 'disabled');
+                    $form.trigger('submit');
+                })
+                .modal('show');
+        }
+    });
+
+    // Initialization of Datatables
+    let table = new DataTable('#list-of-users', {
+        // config options...
+    });
+} );
+
